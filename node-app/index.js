05c34e32095512792a7e3b0694feb6f0b97c4dad
Harsh <kumarharsh93982@gmail.com>
2026-07-07 17:56:57 +0530

    With gauge functionality inc and dec

--- node-app/index.js
+++ node-app/index.js
@@ -2,11 +2,23 @@ import express from "express";
 import morgan from "morgan";
 import responseTime from "response-time";
 import counterMetric from "./counter.helper.js";
+import gaugeMetric from "./gauge.helper.js";
 import masterRouter from "./routes/index.js";
 
 const app = express();
 
 const middleware = (req, res, next) => {
+
+    if (req.originalUrl === "/api/v1/metrics") {
+        return next();
+    }
+
+    // Increase the gauge value
+    gaugeMetric.inc({
+        method: req.method,
+        route: req.originalUrl,
+    });
+
     res.on("finish", () => {
         // Increase the counter.
         counterMetric.inc({
@@ -14,6 +26,12 @@ const middleware = (req, res, next) => {
             route: req.originalUrl,
             status_code: res.statusCode
         });
+
+        // Decrease the gauge value
+        gaugeMetric.dec({
+            method: req.method,
+            route: req.originalUrl,
+        })
     });
     next();
 }
@@ -28,7 +46,10 @@ app.use(middleware);
 
 app.use("/api/v1" ,masterRouter);
 
-app.get("/", (req, res) => {z
+app.get("/", async (req, res) => {
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
     res.status(200).json({
         success: true,
         message: "This is the home page"
